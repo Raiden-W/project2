6ef7dc6bdc11eb165c689c6b94059bf69065b687
Zijun Wang <zijunwang2020@gmail.com>
2023-04-27 18:00:17 +0100

    done Nostalgic Item model, part of create item routes, part of create item page

--- models/User.model.js
+++ models/User.model.js
@@ -1,33 +1,33 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose')
 
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
-const userSchema = new Schema(
+const user = new Schema(
   {
     username: {
       type: String,
       trim: true,
       required: true,
-      unique: true
+      unique: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
       lowercase: true,
-      trim: true
+      trim: true,
     },
     password: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
 
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`    
-    timestamps: true
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
   }
-);
+)
 
-const User = model("User", userSchema);
+const User = model('User', user)
 
-module.exports = User;
+module.exports = User
--- routes/items.routes.js
+++ routes/items.routes.js
@@ -1,26 +1,38 @@
 const express = require('express')
 const router = express.Router()
 
+const NostalgicItem = require('../models/NostalgicItem.model.js')
+
 /* GET home page */
-router.get('/nostalgia-lib', (req, res, next) => {
+router.get('/nostalgia-lib', async (req, res, next) => {
+  // const allItems = await NostalgicItem.find()
   const allItems = [
     {
       name: 'Furby',
-      imgUrl:
+      imgUrl: [
         'https://i.etsystatic.com/7863475/r/il/1b7911/3843664499/il_570xN.3843664499_36iv.jpg',
+      ],
       shortInfo: 'Furby talks Furbish.',
       _id: '1234',
       collectedBy: ['123', '456'],
     },
     {
       name: 'Nokia 3310',
-      imgUrl: 'https://spycraft.b-cdn.net/wp-content/uploads/2019/12/products-188-420x420.jpg',
+      imgUrl: ['https://spycraft.b-cdn.net/wp-content/uploads/2019/12/products-188-420x420.jpg'],
       shortInfo: 'What an acient Nokia.',
       _id: '4567',
       collectedBy: ['123', '456', '789'],
     },
   ]
   res.render('contents/nostalgia-lib', { allItems })
+})
+
+router.get('/create-item', (req, res, next) => {
+  res.render('contents/create-item')
+})
+
+router.post('/create-item', (req, res, next) => {
+  console.log(req.body)
 })
 
 module.exports = router
