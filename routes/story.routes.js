ac36a95e8d4d180c39955c445b95d92c3bd409ba
sarahpst <111440718+sarahpst@users.noreply.github.com>
2023-04-27 23:46:10 +0200

    updated wording

--- routes/story.routes.js
+++ routes/story.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get ('/', (req, res) => {  
-    res.render('story/list'); 
+    res.render('story/lib'); 
 });
 
 router.get ('/add', (req, res) => {  
