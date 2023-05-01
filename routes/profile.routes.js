3844b0d629c1a7a012701dc39403558caeaf7ffd
sarahpst <111440718+sarahpst@users.noreply.github.com>
2023-05-01 02:39:28 +0200

    stories for objects, stories appears on profile by date, story page with latest stories

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const { isLoggedIn } = require('../middleware/route-guards')
+const Story = require('../models/Story.model')
 
-router.get('/', isLoggedIn, (req, res) => {
-  res.render('profile')
+router.get('/', isLoggedIn, async (req, res) => {
+  //stories
+  let stories = []
+  stories = await Story.find({ createdBy: req.session.user._id })
+    .sort({ createdAt: -1 })
+    .populate('createdBy', 'username')
+    .populate('itemId', 'imgUrl')
+  res.render('profile', { stories })
 })
+
+
+
 
 module.exports = router
--- routes/stories.routes.js
+++ routes/stories.routes.js
@@ -1,19 +1,50 @@
 const express = require('express')
 const router = express.Router()
 const { isLoggedIn } = require('../middleware/route-guards')
+const Story = require('../models/Story.model')
 
-router.get('/', (req, res) => {
-  res.render('contents/stories')
+
+router.get('/', async (req, res) => {
+  let stories = []
+  stories = await Story.find()
+    .sort({ createdAt: -1 })
+    .populate('createdBy', 'username')
+    .populate('itemId', 'imgUrl')
+  res.render('contents/stories', { stories })
 })
 
 router.get('/:itemId/stories', (req, res) => {
-  console.log(req.params.contentId)
   res.render('contents/stories')
 })
 
 router.get('/:itemId/create-story', isLoggedIn, (req, res) => {
-  console.log(req.params.contentId)
   res.render('contents/create-story')
 })
+
+router.post('/:itemId/create-story', async (req, res) => {
+  try {
+    const text = req.body.text
+    const createdBy = req.session.user._id
+    const { itemId } = req.params
+
+    // Check if current user has created an other story
+    let isExist = await Story.findOne({ itemId, createdBy })
+    console.log("isExist", isExist)
+    if (!isExist) {
+      await Story.create({ text, createdBy, itemId })
+      res.redirect('/profile')
+    }
+    else {
+      console.log('You already shared your story about this item !')
+      res.redirect('/nostalgia-lib')
+    }
+
+  }
+  catch (error) {
+    console.log('error in the create story route', error)
+  }
+})
+
+
 
 module.exports = router
